a30a8072a8199a2ea97cb6a578e3aae494996e1b
Steve Turley <steve@turleylabs.com>
2021-01-18 09:52:32 -0600

    Initial commit

---
+++ src/test/java/beyond/gilded/rose/GildedRoseTest.java
@@ -0,0 +1,14 @@
+package beyond.gilded.rose;
+
+import org.approvaltests.combinations.CombinationApprovals;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseTest {
+    @Test
+    public void whatDoesGildedRoseDo() {
+    }
+}
